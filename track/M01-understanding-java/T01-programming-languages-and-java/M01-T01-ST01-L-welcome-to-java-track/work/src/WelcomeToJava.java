6f95edce1f95cfbda2b1ded825a66318430a9619
Karthik <karthikeyanramakingam549@gmail.com>
2026-07-30 13:10:32 +0530

    rolex

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,11 +3,11 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         //Write your code here
+        System.out.println("INAIKU DAY 1 SCRATCHLA IRUDHU ARAMBIKURA ");
         System.out.println("Welcome to the java track");
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now i am learning how to express that logic usin java.");
-        System.out.println("this is my day 1");
         System.out.println("now i know how to use git and github");
-
+        System.out.println("Intha rendu peroda thalaiya kondu varavanuku lifetime settlement ra.");
     }
 }
