ca3e27ae564c51815a201d49bdc3dd5aff68a3b2
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-04 12:29:28 +0530

    if,else if,else

---
+++ practice/prog2.java
@@ -0,0 +1,14 @@
+public class prog2{
+    public static void main(String[] args) {
+        int num=0;
+        if (num>0)
+        {
+            System.out.println("The number is positive");  }
+        else if (num < 0) {
+            System.out.println("The number is negative");  
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
