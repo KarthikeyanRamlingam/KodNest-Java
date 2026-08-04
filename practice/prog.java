6f7d7ddd8e7f66f1346a1cba915fa6fdcdd0bc1f
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-04 11:33:02 +0530

    pratice01

---
+++ practice/prog.java
@@ -0,0 +1,12 @@
+public class prog{
+    public static void main(String [] args){
+        System.out.println("Monday");
+        System.out.print("Tuseday");
+        System.out.println("Wendsday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+
+    }
+}
