4ca49f4da87b358035fcddf4dcc865a6c2c631d3
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-04 22:06:19 +0530

    String learner profile

---
+++ track/M02-inro-java/pract04.java
@@ -0,0 +1,10 @@
+
+public class pract04 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
