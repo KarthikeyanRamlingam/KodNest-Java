1a282a8ac7db85a3139540d86df4e4921a50315c
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-04 20:03:34 +0530

    weekly plan

---
+++ track/M02-inro-java/pract03.java
@@ -0,0 +1,16 @@
+
+public class pract03 {
+
+    public static void main(String[] args) {
+        int ap = 1;
+        int num = 5;
+        int java = 2;
+        int week = java * num;
+        int apweek = ap * num;
+        int total = week + apweek;
+        System.out.println("Java: " + week);
+        System.out.println("Aptitude: " + apweek);
+        System.out.println("Total: " + total);
+    }
+
+}
