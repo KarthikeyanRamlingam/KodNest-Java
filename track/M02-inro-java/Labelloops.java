1e4c6b8fc55ac13b099a0fda2502ec1c52fe9047
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-06 13:06:15 +0530

    label

---
+++ track/M02-inro-java/Labelloops.java
@@ -0,0 +1,13 @@
+public class Labelloops {
+    public static void main(String[] args) {
+        outer: for (int i = 1; i <= 3; i++) {
+            inner: for (int j = 1; j <= 5; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.out.println("i: " + i + " j: " + j);
+            }
+            System.out.println();
+        }
+    }
+}
