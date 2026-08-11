6bd13042df2e672a9961929ef492347706481b3f
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-11 11:12:56 +0530

    reference map

---
+++ track/M02-inro-java/Reference.java
@@ -0,0 +1,25 @@
+class Emp{
+    int id;
+    String name;
+    void action()
+    {
+        System.out.println("working");    }
+}
+public class Reference{
+    public static void main(String[] args) {
+        Emp e1=new Emp();
+        e1.id=01;
+        e1.name="anu";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+
+        Emp e2;//new ref
+        e2=e1;
+        e2.id=02;
+        e2.name="arun";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+    }
+}
