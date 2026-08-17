644b21990f8533bceb5d98cdcdc214b4e01d72e5
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-17 12:52:50 +0530

    array

---
+++ track/M02-inro-java/1708.java/arr.java
@@ -0,0 +1,22 @@
+import  java.util.Scanner;
+public class arr{
+    public static void main(String []args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("size: ");
+        int n=scan.nextInt();
+        int[]arr=new int[n];
+
+        for(int i=0;i<n;i++ )
+        {
+            System.out.print("Enter values: ");
+            arr[i]=scan.nextInt();
+        }
+        System.out.println("array values: ");
+        for(int i=0;i<n;i++ )
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
