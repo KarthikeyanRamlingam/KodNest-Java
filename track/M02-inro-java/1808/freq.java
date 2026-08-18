317b99dbb91836b97bf015b4935fac41d8a3b7b1
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-18 12:36:17 +0530

    find count

---
+++ track/M02-inro-java/1808/freq.java
@@ -0,0 +1,23 @@
+
+class find{
+    int count=0;
+    void count(int arr[],int key)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+                count++;
+
+            }
+        }
+        System.out.println(key+":"+count);
+    }
+}
+public class freq{
+    public static void main(String[] args) {
+        int arr[]={10,20,10,30,10,30};
+        find f=new find();
+        f.count(arr,30);
+    }
+}
