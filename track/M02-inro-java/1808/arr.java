231d81d0fceaf15e38222e4e25700b834eb48b88
Karthik <karthikeyanramakingam549@gmail.com>
2026-08-18 11:05:19 +0530

    arr

--- track/M02-inro-java/1808/arr.java
+++ track/M02-inro-java/1808/arr.java
@@ -12,5 +12,12 @@ public class arr{
         {
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+        }
+        System.out.println("sum: "+sum);
     }
 }
